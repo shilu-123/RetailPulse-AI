632762859b3dc48f4db76402bcf5efa0c6f34aeb
shilu-123 <pk5914274@gmail.com>
2026-07-12 22:17:29 +0530

    Use environment variable for API URL

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "https://retailpulse-ai-m7se.onrender.com/api"
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
